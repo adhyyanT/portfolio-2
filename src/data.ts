c9d8f0c042c9315f01cc11fc9eb318b7094f4992
Adhyyan Tripathi <adhyyan321@gmail.com>
2025-09-07 13:37:09 +0530

    updates desc

--- src/data.ts
+++ src/data.ts
@@ -1,90 +1,91 @@
 export const DATA = {
-  profile: {
-    subtitle:
-      "Software Engineer | React | Express | TypeScript | MySQL | Kafka | Microservices",
-    title: "Full-Stack Developer",
-    location: "📍 Mumbai, India 🇮🇳",
-  },
-  about: `Software engineer with expertise in React, TypeScript, Express, and building user-focused, scalable, data-driven applications. Worked
+	profile: {
+		subtitle:
+			"Software Engineer | React | Express | TypeScript | MySQL | Kafka | Microservices",
+		title: "Full-Stack Developer",
+		location: "📍 Mumbai, India 🇮🇳",
+	},
+	about: `Software engineer with expertise in React, TypeScript, Express, and building user-focused, scalable, data-driven applications. Worked
 across finance and advertising domains to help businesses bring products to market faster and more efficiently. Proficient in working
 across the full stack and aligning technical solutions with business goals.`,
-  skillSections: [
-    {
-      title: "Programming Languages",
-      skills: ["TypeScript", "Javascript", "Python", "Java"],
-    },
-    {
-      title: "Frameworks",
-      skills: ["ReactJS", "Flask", "Angular", "StoryBook"],
-    },
-    {
-      title: "Technologies",
-      skills: ["NodeJS", "REST APIs", "Microservices", "Kafka", "Redux", "AWS"],
-    },
-    {
-      title: "Databases",
-      skills: ["MySQL", "MongoDB"],
-    },
-  ],
-  experiences: [
-    {
-      title: "Software Engineer",
-      location: "V2Solutions, Mumbai",
-      startDate: "10/15/2023",
-      endDate: undefined,
-      logo: "https://4ep09v5dco.ufs.sh/f/ObbFGHI3vFYcQJEGSGcjVqg5euSBxi2kfInt71TdsC89WoJ6",
-      bullets: [
-        "Migrated a legacy mortgage underwriting tool into a responsive Angular web app to support real-time financial decision-making, improving analyst efficiency by 66%.",
-        "Contributed to an internal UI component library built with Angular, TypeScript, and Storybook.",
-        "Developed interactive and accessible web pages from Figma designs, ensuring responsiveness across devices.",
-        "Implemented APIs using Python (Flask), and MySQL to handle long-duration concurrent modification on the same database record.",
-        "Communicated with stakeholders and collaborated with cross-functional teams to gather, analyse, and align requirements for efficient project execution.",
-      ],
-      skills: [
-        "Angular",
-        "TypeScript",
-        "Javascript",
-        "HTML5",
-        "CSS3",
-        "StoryBook",
-        "Python",
-        "MySQL",
-      ],
-      tagIndex: 4,
-    },
-    {
-      title: "Analyst",
-      location: "Capgemini, Bengaluru",
-      startDate: "10/19/2021",
-      endDate: "11/29/2022",
-      logo: "https://4ep09v5dco.ufs.sh/f/ObbFGHI3vFYciN4oxZ1vRapPGKbVo0e4qJz72Xt5kAfHSF6r",
-      bullets: [
-        "Developed applications from the ground up using Angular to track lead generation and manage an admin portal, integrating Microsoft Azure services like Application Insights for logging.",
-        "Engineered a highly customisable landing page that allows modifications through an intuitive user interface in the admin portal using Angular Material.",
-        "Aggregated multiple APIs into a single REST endpoint, reducing network calls by 80% and improving application efficiency using Python.",
-        "Created multiple RESTful endpoints in Flask, including dynamic QR code generation with embedded URLs, and optimised resource usage by implementing image compression, reducing storage costs by 20%.",
-        "Actively collaborated with the team in an Agile environment, participating in daily stand-ups, sprint planning, and retrospectives to ensure efficient project execution and continuous improvement.",
-      ],
-      skills: [
-        "Angular",
-        "TypeScript",
-        "Flask",
-        "AWS",
-        "REST API",
-        "Agile",
-        "Angular Material",
-        "Python",
-      ],
-      tagIndex: 4,
-    },
-  ],
-  educations: [
-    {
-      title: "B.Tech in Computer Science Engineering",
-      college: "Sikkim Manipal Institute of Technology",
-      startDate: "07/23/2017",
-      endDate: "10/01/2021",
-      logo: "https://4ep09v5dco.ufs.sh/f/ObbFGHI3vFYc5PcduEZbl167s9vmErTjtqi8XAfDJH43Khdn",
-    },
-  ],
+	skillSections: [
+		{
+			title: "Programming Languages",
+			skills: ["TypeScript", "Javascript", "Python", "Java"],
+		},
+		{
+			title: "Frameworks",
+			skills: ["ReactJS", "Express.js", "Angular", "StoryBook"],
+		},
+		{
+			title: "Technologies",
+			skills: ["NodeJS", "REST APIs", "Microservices", "Kafka", "Redux", "AWS"],
+		},
+		{
+			title: "Databases",
+			skills: ["MySQL", "MongoDB"],
+		},
+	],
+	experiences: [
+		{
+			title: "Software Engineer",
+			location: "V2Solutions, Mumbai",
+			startDate: "10/15/2023",
+			endDate: undefined,
+			logo: "https://4ep09v5dco.ufs.sh/f/ObbFGHI3vFYcQJEGSGcjVqg5euSBxi2kfInt71TdsC89WoJ6",
+			bullets: [
+				"Migrated a legacy mortgage underwriting tool into a responsive React web app to support real-time financial decision-making, improving analyst efficiency by 66%.",
+				"Implemented APIs using TypeScript, and MySQL to handle long-duration concurrent modification on the same database record.",
+				"Built and integrated backend services with Express.js, enabling automated generation of fraud detection reports and reducing manual review time.",
+				"Contributed to an internal UI component library built with React, TypeScript, and Storybook.",
+				"Developed interactive and accessible web pages from Figma designs, ensuring responsiveness across devices.",
+				"Communicated with stakeholders and collaborated with cross-functional teams to gather, analyse, and align requirements for efficient project execution.",
+			],
+			skills: [
+				"React",
+				"TypeScript",
+				"Javascript",
+				"HTML5",
+				"CSS3",
+				"StoryBook",
+				"Express.js",
+				"MySQL",
+			],
+			tagIndex: 4,
+		},
+		{
+			title: "Analyst",
+			location: "Capgemini, Bengaluru",
+			startDate: "10/19/2021",
+			endDate: "11/29/2022",
+			logo: "https://4ep09v5dco.ufs.sh/f/ObbFGHI3vFYciN4oxZ1vRapPGKbVo0e4qJz72Xt5kAfHSF6r",
+			bullets: [
+				"Developed applications from the ground up using Angular to track lead generation and manage an admin portal, integrating services like Amazon S3.",
+				"Engineered a highly customisable landing page that allows modifications through an intuitive user interface in the admin portal using Angular Material.",
+				"Aggregated multiple APIs into a single REST endpoint, reducing network calls by 80% and improving application efficiency using Express.js.",
+				"Created multiple RESTful endpoints in Express, including dynamic QR code generation with embedded URLs, and optimized resource usage by implementing image compression, reducing storage costs by 20%.",
+				"Actively collaborated with the team in an Agile environment, participating in daily stand-ups, sprint planning, and retrospectives to ensure efficient project execution and continuous improvement.",
+			],
+			skills: [
+				"React.js",
+				"Angular",
+				"Angular Material",
+				"TypeScript",
+				"Express.js",
+				"AWS",
+				"REST API",
+				"Agile",
+			],
+			tagIndex: 4,
+		},
+	],
+	educations: [
+		{
+			title: "B.Tech in Computer Science Engineering",
+			college: "Sikkim Manipal Institute of Technology",
+			startDate: "07/23/2017",
+			endDate: "10/01/2021",
+			logo: "https://4ep09v5dco.ufs.sh/f/ObbFGHI3vFYc5PcduEZbl167s9vmErTjtqi8XAfDJH43Khdn",
+		},
+	],
 };
